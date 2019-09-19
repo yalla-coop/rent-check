5b67dcefa227f67060342e953d12bb42738bc337
Simon Dupree <dupreenator@gmail.com>
2019-09-19 14:48:04 +0200

    added search feature to user component

--- src/components/Admin/Users/AllUsers.js
+++ src/components/Admin/Users/AllUsers.js
@@ -1,11 +1,96 @@
 import React, { Component } from 'react'
+import { Table, Input, Icon, Button } from "antd";
+import usersCol from './UsersColumns';
+
+// fake data
+import { dataSource } from './dummyUsers'
 
 export default class AllUsers extends Component {
+ state = {
+    data: [],
+    visible: false,
+    id: "",
+    searchText: ""
+  };
+
+  getColumnSearchProps = dataIndex => ({
+    filterDropdown: ({
+      setSelectedKeys,
+      selectedKeys,
+      confirm,
+      clearFilters
+    }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={node => {
+            this.searchInput = node;
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e =>
+            setSelectedKeys(e.target.value ? [e.target.value] : [])
+          }
+          onPressEnter={() => this.handleSearch(selectedKeys, confirm)}
+          style={{ width: 188, marginBottom: 8, display: "block" }}
+        />
+        <Button
+          type="primary"
+          onClick={() => this.handleSearch(selectedKeys, confirm)}
+          icon="search"
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          Search
+        </Button>
+        <Button
+          onClick={() => this.handleReset(clearFilters)}
+          size="small"
+          style={{ width: 90 }}
+        >
+          Reset
+        </Button>
+      </div>
+    ),
+    filterIcon: filtered => (
+      <Icon
+        type="search"
+        style={{ fontSize: "20px", color: filtered ? "#1890ff" : undefined }}
+      />
+    ),
+    onFilter: (value, record) => {
+      return record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes(value.toLowerCase());
+    },
+    onFilterDropdownVisibleChange: visible => {
+      if (visible) {
+        setTimeout(() => this.searchInput.select());
+      }
+    }
+  });
+
+  handleSearch = (selectedKeys, confirm) => {
+    confirm();
+    this.setState({ searchText: selectedKeys[0] });
+  };
+
+  handleReset = clearFilters => {
+    clearFilters();
+    this.setState({ searchText: "" });
+  };
+
   render() {
     return (
-      <div>
-        <h1>All Users Component</h1>
-      </div>
+    <Table
+      columns={usersCol({
+        getColumnSearchProps: this.getColumnSearchProps,
+        searchText: this.state.searchText
+      })}
+      dataSource={dataSource}
+      style={{ backgroundColor: "#ffffff" }}
+      bordered
+    />
     )
   }
 }
---
+++ src/components/Admin/Users/UsersColumns.js
@@ -0,0 +1,74 @@
+import React from "react";
+import Highlighter from "react-highlight-words";
+
+export default ({
+  getColumnSearchProps,
+  searchText
+}) => {
+  const tableColumns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: text => (
+        <span style={{ fontWeight: "700" }}>
+          <Highlighter
+            highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+            searchWords={[searchText]}
+            autoEscape
+            textToHighlight={text.toString()}
+          />
+        </span>
+      ),
+      ...getColumnSearchProps("name")
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+      render: text => (
+        <Highlighter
+          highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text.toString()}
+        />
+      ),
+      ...getColumnSearchProps("email")
+    },
+    {
+      title: "Level",
+      dataIndex: "level",
+      key: "level",
+      render: text => (
+        <span style={{ fontWeight: "700" }}>
+          <Highlighter
+            highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+            searchWords={[searchText]}
+            autoEscape
+            textToHighlight={text.toString()}
+          />
+        </span>
+      ),
+      ...getColumnSearchProps("level")
+    },
+    {
+      title: "Actions",
+      dataIndex: "actions",
+      key: "actions",
+      render: text => (
+        <span style={{ fontWeight: "700" }}>
+          <Highlighter
+            highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
+            searchWords={[searchText]}
+            autoEscape
+            textToHighlight={text.toString()}
+          />
+        </span>
+      ),
+      ...getColumnSearchProps("actions")
+    },
+  ];
+
+  return tableColumns;
+}
