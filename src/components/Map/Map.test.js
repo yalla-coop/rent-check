bf46315ccc89edcb2f80d43b36572332dd0e2dc3
Michael Watts <hello@michaelwatts.co>
2019-09-20 12:19:05 +0100

    rename props in Map.test

--- src/components/Map/Map.test.js
+++ src/components/Map/Map.test.js
@@ -1,33 +1,33 @@
-import React from "react";
-import Map from ".";
+import React from 'react';
 import { render, cleanup } from "@testing-library/react";
+import Map from ".";
 
 const markers = [
   {
     key: 2,
-    postcode: "N4 3HH",
-    address: "15 Something Street",
-    price_sqft: 9.5,
-    use_class: "A1",
-    date_of_last_rent_review: "2018-05-23",
-    name: "Wish Fashion",
-    geolocation: "[51.564261,-0.108292]",
+    postcode: 'N4 3HH',
+    address: '15 Something Street',
+    priceSqFt: 9.5,
+    useClass: 'A1',
+    lastRentReview: '2018-05-23',
+    name: 'Wish Fashion',
+    geoLocation: '[51.564261,-0.108292]',
   },
   {
     key: 3,
-    postcode: "N4 3HQ",
-    address: "14 Goodwin Street, London",
-    price_sqft: 15,
-    use_class: "A3",
-    date_of_last_rent_review: "2018-04-01",
-    name: "Pure Cyprus",
-    geolocation: "[51.564162,-0.107777]",
+    postcode: 'N4 3HQ',
+    address: '14 Goodwin Street, London',
+    priceSqFt: 15,
+    useClass: 'A3',
+    lastRentReview: '2018-04-01',
+    name: 'Pure Cyprus',
+    geoLocation: '[51.564162,-0.107777]',
   },
 ];
 
-const wrongDate = "12/25/2018";
+const wrongDate = '12/25/2018';
 
-it("Map renders without crashing", () => {
+it('Map renders without crashing', () => {
   render(<Map markers={markers} />);
 });
 
