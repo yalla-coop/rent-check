3899cd4f540021852a59eee7d458857e1fb7c3f5
FarahZaqout <farah.zaqout@gmail.com>
2019-09-25 02:12:26 +0300

    adds auth0 and private route to the app routes #1,#2,#3

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,45 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
 // import components here
 import MapInterface from './MapInterface';
 import Admin from './Admin';
+import { useAuth0, Auth0Provider } from './Auth0Login/Auth0Wrapper';
+import PrivateRoute from './Auth0Login/PrivateRoute';
+// import { routes } from '../constants/adminRoutes';
 
-export default function index() {
+const onRedirectCallback = appState => {
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
+function Index() {
+  const { loading } = useAuth0();
+  if (loading) return <h2>Loading...</h2>;
   return (
     <Switch>
-      <Route exact path="/" component={MapInterface} />
-      <Route path="/admin" component={Admin} />
+      <PrivateRoute exact path="/" component={MapInterface} />
+      <PrivateRoute path="/admin" component={Admin} />
     </Switch>
   );
 }
+
+const Routes = () => {
+  return (
+    <Auth0Provider
+      domain={process.env.AUTH0_DOMAIN}
+      client_id={process.env.AUTH0_CLIENT_ID}
+      redirect_uri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      <Index />
+    </Auth0Provider>
+  );
+};
+
+export default Routes;
