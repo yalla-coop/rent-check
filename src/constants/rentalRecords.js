c036028fadf463436fd08f76239542508273644a
amusameh <abdalsamad.y.m@gmail.com>
2019-09-26 09:42:20 +0300

    refactor the enum types into constants rentalRecord modal

--- src/constants/rentalRecords.js
+++ src/constants/rentalRecords.js
@@ -5,3 +5,8 @@ export const status = {
 };
 
 export const statusEnum = Object.values(status);
+
+export const landlordTenantsActEnum = ['Yes', 'No', 'Not sure'];
+export const specificationEnum = ['Refurbished', 'Shell', 'Other / Not sure'];
+export const useClassEnum = ['A1', 'A3', 'B1', 'B2', 'B8', 'D1', 'D2', 'Other'];
+export const restrictedEnum = ['Restricted', 'Unrestricted', 'Not sure'];
