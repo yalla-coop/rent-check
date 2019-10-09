0739f7896d7870b30f7c36b3540a036bcf61b6a8
amusameh <abdalsamad.y.m@gmail.com>
2019-10-09 19:14:22 +0300

    update build script to fix travis errors

--- src/lambda/database/data/index.js
+++ src/lambda/database/data/index.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import dbConnection from '../dbConnection';
 import resetDb from '../resetDb';
 import users from './users';
@@ -18,5 +19,20 @@ const buildData = () =>
       throw err;
     }
   });
+
+if (process.env.NODE_ENV !== 'test') {
+  buildData()
+    .then(async () => {
+      // eslint-disable-next-line no-console
+      console.log('Done!: Dev DB has been built successfully');
+      // close the connection after build
+      await mongoose.disconnect();
+      process.exit(0);
+    })
+    .catch(err => {
+      // eslint-disable-next-line no-console
+      console.log('err', err);
+    });
+}
 
 export default buildData;
