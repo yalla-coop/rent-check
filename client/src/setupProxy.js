1c1ebdb40b3ba2858bef70297bf76eb5d89429be
Michael Watts <hello@michaelwatts.co>
2019-10-11 14:57:12 +0100

    amend syntax

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,8 +2,11 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use('/api', proxy({
-    target: 'http://localhost:8000',
-    changeOrigin: true,
-  }));
+  app.use(
+    '/api',
+    proxy({
+      target: 'http://localhost:8000',
+      changeOrigin: true,
+    })
+  );
 };
