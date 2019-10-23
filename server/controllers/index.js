9639e88e2c31a6cc89f45e843dc53222300a4c4b
Michael Watts <hello@michaelwatts.co>
2019-10-23 10:58:06 +0100

    use express.Router

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { getLocations, addLocation } = require("./locations");
 const admin = require("./admin");
-const router = express();
+const router = express.Router();
 
 router.get("/locations", getLocations);
 router.post("/locations", addLocation);
