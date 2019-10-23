e88a956cd1b9d2d606078b3d64a728cf9f6192e8
Michael Watts <hello@michaelwatts.co>
2019-10-23 16:58:05 +0100

    add styling

--- client/src/components/RequestStreetRep/index.js
+++ client/src/components/RequestStreetRep/index.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { Button } from 'antd';
+
+import styled from "styled-components";
 
 export default function RequestStreetRep(props) {
   return (
-    <section>
-    <h1>Become a street rep!</h1>
-    <p>Impress all your friends</p>
-    </section>
+    <div className="w-100 bg-dark-pink white ph2 dt">
+      <div className="dtc v-mid tc pt5 vh-100">
+        <section className="measure lh-copy db mh-auto">
+          <h1>Become a street rep!</h1>
+          <p>Make your community stronger by becoming a street rep for Guardians of 
+          the Arches and East End Trades Guild.</p>
+          <p>Street reps play a crucial role in the Rent Check app by verifying that users
+          and data added to the app are trustworthy, keeping our community safe from 
+          those who might try to sneak their way in to access our data.</p>
+          <p>To become a street rep for your local area, you will need to request Street
+          Rep status by clicking the button below, and then you will need to speak to an
+          existing street rep in person to complete your request.</p>
+          <Button>Be a street rep</Button>
+          <Button>View a list of street reps</Button>
+        </section>
+      </div>
+    </div>
   )
 }
